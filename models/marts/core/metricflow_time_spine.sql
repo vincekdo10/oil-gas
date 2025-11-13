98ed8873bc1097560f652c5befbb7b28a5874d14
vincekdo10 <vdo1017@gmail.com>
2025-11-13 16:01:59 -0600

    fixing dates

--- models/marts/core/metricflow_time_spine.sql
+++ models/marts/core/metricflow_time_spine.sql
@@ -9,7 +9,7 @@ with date_spine as (
     {{ dbt_utils.date_spine(
         datepart="day",
         start_date="cast('2020-01-01' as date)",
-        end_date="cast('2030-12-31' as date)"
+        end_date="cast('2025-12-31' as date)"
     ) }}
 )
 
